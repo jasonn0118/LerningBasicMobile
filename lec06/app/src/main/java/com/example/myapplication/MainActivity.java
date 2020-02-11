fc74338823ff54d26b695794e2ba4d3e7f690489
Jasonn0118 <tlswoals2006@gmail.com>
2020-02-11 11:51:58 -0800

    This is Audio file Insertion

--- lec06/app/src/main/java/com/example/myapplication/MainActivity.java
+++ lec06/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,10 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> songNameList = new ArrayList<>();
     List<Integer> songPicList = new ArrayList<>();
+    List<Integer> songList = new ArrayList<>();
+    MediaPlayer currentSong = new MediaPlayer();
 
     private void addItems(){
         songNameList = Arrays.asList("Drums", "Ukulele","Bag Pipes");
         songPicList = Arrays.asList(R.drawable.drums,R.drawable.ukulele,R.drawable.bagpipes);
+        songList = Arrays.asList(R.raw.drums,R.raw.ukulele,R.raw.bagpipes);
     }
 
     @Override
@@ -23,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addItems();
+
+        final myCustomAdapter myAdapter = new myCustomAdapter(songNameList,songPicList);
+        ListView listViewItems = findViewById(R.id.listViewSong);
+        listViewItems.setAdapter(myAdapter);
+        //On click change.
+        listViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(myAdapter.getCurrentPlayIndex() == position){
+                    if(currentSong != null && currentSong.isPlaying()){
+                        currentSong.pause();
+                        myAdapter.setPlayStatus(false);
+                    } else if(currentSong!=null && !currentSong.isPlaying()){
+                        currentSong.start();//resume play.
+                        myAdapter.setPlayStatus(true);
+                    }
+                } else {
+                    //first stop current song if playing.
+                    if(currentSong != null && currentSong.isPlaying()){
+                        currentSong.stop();
+                        //need to prepare if restarting the same media player objedct;
+                    }
+                    //already prepared
+                    currentSong = MediaPlayer.create(MainActivity.this, songList.get(position));
+                    currentSong.start();
+                    myAdapter.setCurrentPlayIndex(position);
+                    myAdapter.setPlayStatus(true);
+                }
+            }
+        });
     }
 }
--- lec06/app/src/main/java/com/example/myapplication/myCustomAdapter.java
+++ lec06/app/src/main/java/com/example/myapplication/myCustomAdapter.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,25 +21,60 @@ public class myCustomAdapter extends BaseAdapter {
     public myCustomAdapter(List<String> adapterSongNameList, List<Integer> adapterSongPicList) {
         this.adapterSongNameList = adapterSongNameList;
         this.adapterSongPicList = adapterSongPicList;
+        currentPlayIndex = -1;
+        playStatus = false;
+    }
+
+    public int getCurrentPlayIndex() {
+        return currentPlayIndex;
+    }
+
+    public void setCurrentPlayIndex(int currentPlayIndex) {
+        this.currentPlayIndex = currentPlayIndex;
+        notifyDataSetChanged();//This is absolutely required
+    }
+
+    public boolean isPlayStatus() {
+        return playStatus;
+    }
+
+    public void setPlayStatus(boolean playStatus) {
+        this.playStatus = playStatus;
+        notifyDataSetChanged();//This is absolutely required
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return adapterSongNameList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return adapterSongNameList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView==null) {
+            LayoutInflater myLayoutFlater = LayoutInflater.from(parent.getContext());
+            convertView = myLayoutFlater.inflate(R.layout.layout_myinstruments, parent,false);
+        }
+        ImageView imgViewItem = convertView.findViewById(R.id.imageViewItem);
+        TextView textViewItem = convertView.findViewById(R.id.textViewItem);
+        ImageView imgViewPlay = convertView.findViewById(R.id.imageViewPlay);
+        textViewItem.setText(adapterSongNameList.get(position));
+        imgViewItem.setImageResource(adapterSongPicList.get(position));//Caution.
+
+        if(playStatus && position==currentPlayIndex){
+            imgViewPlay.setImageResource(R.drawable.pause);
+        } else{
+            imgViewPlay.setImageResource(R.drawable.play);
+        }
+        return convertView;
     }
 }
